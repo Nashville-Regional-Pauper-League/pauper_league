1791ddc404083d8391974e04a3481f234748db66
David Clark <david.clark.1043@gmail.com>
2026-08-01 09:15:35 -0400

    Formatting change

--- lib/pauper_league/workers/event_data_worker.ex
+++ lib/pauper_league/workers/event_data_worker.ex
@@ -2,6 +2,9 @@ defmodule PauperLeague.Workers.EventDataWorker do
   use Oban.Worker, max_attempts: 1, queue: :event_link
 
   import Ecto.Changeset
+  alias PauperLeague.Repo
+  alias PauperLeague.Stores.RawEvent
+  alias PauperLeague.Stores.RawEventData
 
   def perform(%{args: %{"event_id" => eventlink_id, "store_id" => store_id}}) do
     with {_, resp} <- PauperLeague.EventlinkApi.get_event_info(eventlink_id, store_id),
@@ -11,16 +14,16 @@ defmodule PauperLeague.Workers.EventDataWorker do
       event = resp.body |> get_in(event_info_keys())
 
       if not is_nil(event) do
-        raw_event =
-          PauperLeague.Stores.RawEvent
-          |> PauperLeague.Repo.get_by(eventlink_id: eventlink_id)
+        raw_event = RawEvent |> Repo.get_by(eventlink_id: eventlink_id)
 
         params = %{data: event, event_id: raw_event.id}
         fields = [:data, :event_id]
 
-        %PauperLeague.Stores.RawEventData{}
-        |> cast(params, fields)
-        |> PauperLeague.Repo.insert()
+        with {:ok, _e} <- %RawEventData{} |> cast(params, fields) |> Repo.insert() do
+          raw_event
+          |> change(%{internal_state: "raw_data_event"})
+          |> Repo.update()
+        end
       end
     else
       429 ->
