63025c7d944cfdda48757ca0299094633e8b33f6
David Clark <david.clark.1043@gmail.com>
2026-08-20 00:24:55 -0500

    Adds ca certs for db ssl

--- config/runtime.exs
+++ config/runtime.exs
@@ -28,10 +28,15 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  ca_certs = System.get_env("CA_CERTS")
+
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :pauper_league, PauperLeague.Repo,
-    ssl: true,
+    ssl: [
+      verify: :verify_peer,
+      cacerts: [ca_certs]
+    ],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
