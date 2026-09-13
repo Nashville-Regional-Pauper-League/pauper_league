e38f53d68efe8993ddca1985fef67d369ef73f76
David Clark <david.clark.1043@gmail.com>
2026-09-13 18:10:48 -0500

    No Display Opp Deck when Bye

--- lib/pauper_league/player.ex
+++ lib/pauper_league/player.ex
@@ -143,7 +143,8 @@ defmodule PauperLeague.Player do
         end)
         |> Enum.filter(fn match -> not is_nil(match.opp_player_id) end)
         |> Enum.sort_by(fn match -> [match.total_matches, match.match_wins] end, :desc)
-        |> IO.inspect()
+
+      # |> IO.inspect()
 
       player_details = %{
         record: "#{player.match_wins}-#{player.match_losses}-#{player.match_draws}",
